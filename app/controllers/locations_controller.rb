eab01abe90d4c97743f9023a857f373ad88c8d75
shane wierenga <shane@midwestsignco.com>
2016-03-20 21:10:43 -0400

    added location editing capabilities

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,52 +1,65 @@
 class LocationsController < ApplicationController
-	 def new
-      @location = Location.new
-    end
+  
   def index
-      @contacts = Contact.all
-        if params[:search]
-
-            @contact = Contact.find(params[:search])
-            @locations = Location.matchesContact(params[:search])
-            respond_to do |format|
-               format.html { render "show" }
-               format.js { render "show" }
-            end
-        else
-
-        end
-
-  end
-
-  def show
+    
+    if params[:id]
       @contact = Contact.find(params[:id])
-      @location = Location.matchesContact(params[:id])
-         
-        respond_to do |format|
-           format.html { render "show" }
-           format.js { render "show" }
-        end
-
+      @locations = Location.matchesContact(params[:id])
+      @location = Location.new
+      respond_to do |format|
+        format.html { render "show" }
+        format.js { render "show" }
+      end
+     
+      else
+    end
   end
-	
+  def show
+    
+    @locations = Location.matchesContact(params[:id])
+    @contact = Contact.find(params[:id])
+    @location = Location.new
+    respond_to do |format|
+      format.html { render "show" }
+      format.js { render "show" }
+    end
+  end
+  def new
+    @location = Location.new
+  end
+  def edit
+    @location = Location.find(params[:id])
+  end	
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update(location_params)
+      redirect_to location, format: 'js'
+      
+    else 
+
+    end
+  end
+
   def create
     @contact = Contact.find(params[:contact_id])
     @location = @contact.locations.create(location_params)
     flash[:notice] = "Location was successfully created." if @location.save
     respond_with(@contact)
 	end
- 
   def destroy
     @contact = Contact.find(params[:contact_id])
     @location = @contact.locations.find(params[:id])
     @location.destroy
     redirect_to contact_path(@contact)
   end
+end
+
  
   private
     def location_params
       params.require(:location).permit(:locationName, :Address, :Address1, :City, :StateOrProvince, :PostalCode, :WorkPhone)
     end
-end
+
 
 
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,24 @@
 class PeopleController < ApplicationController
+	def new
+    @person = Person.new
+  end
+  def edit
+    @person = Person.find(params[:id])
+  end	
+  def create
+    @location = Location.find(params[:location_id])
+    @person = @location.people.create(people_params)
+    flash[:notice] = "Person was successfully created." if @person.save
+    respond_with(@location)
+	end
+  def destroy
+    @location = Location.find(params[:location_id])
+    @person = @location.people.find(params[:id])
+    @person.destroy
+    redirect_to location_path(@location)
+  end
+  private
+    def location_params
+      params.require(:person).permit(:FirstName, :LastName, :Title, :MobilePhone, :EmailName)
+    end
 end
