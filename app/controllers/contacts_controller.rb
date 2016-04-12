8610c9d5efadc68b1738f5369f6958f936e8303f
shane wierenga <shane@midwestsignco.com>
2016-04-12 05:29:41 -0400

    added user auth to contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,12 @@
+ 
 class ContactsController < ApplicationController
-    
-	def new
-    	@contact = Contact.new
+    before_action :authenticate_user!
+    layout 'contact'
+    def new
+        @contact = Contact.new
     end
     def edit
-    	@contact = Contact.find(params[:id])
+        @contact = Contact.find(params[:id])
     end
     
     def index
@@ -31,37 +33,37 @@ class ContactsController < ApplicationController
     end
         
     def create
-    	@contact = Contact.new(contact_params)
-    	if @contact.save
-    		respond_to do |format|
-                format.js { render "show" }
-            end
-    	else
-    		
-    	end
-    end
-
-    def update
-    	@contact = Contact.find(params[:id])
-    	if @contact.update(contact_params)
+        @contact = Contact.new(contact_params)
+        if @contact.save
             respond_to do |format|
                 format.js { render "show" }
             end
-    	
+        else
+            
+        end
+    end
+
+    def update
+        @contact = Contact.find(params[:id])
+        if @contact.update(contact_params)
+            respond_to do |format|
+                format.js { render "show" }
+            end
+        
         else 
             
-    	end
+        end
     end
 
     def destroy
-    	@contact = Contact.find(params[:id])
-    	@contact.destroy
+        @contact = Contact.find(params[:id])
+        @contact.destroy
 
-    	redirect_to contacts_path
+        redirect_to contacts_path
     end
     
     private
     def contact_params
-    	params.require(:contact).permit(:CompanyName, :Address, :Address1, :City, :StateOrProvince, :PostalCode, :WorkPhone, :FaxNumber)
+        params.require(:contact).permit(:CompanyName, :Address, :Address1, :City, :StateOrProvince, :PostalCode, :WorkPhone, :FaxNumber)
     end
 end
