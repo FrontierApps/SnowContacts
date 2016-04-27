da9f78e116ae1046a2a9ebed015c0607d078aae4
shane wierenga <shane@midwestsignco.com>
2016-04-26 21:53:19 -0400

    signs controller additions

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -1,4 +1,11 @@
 class SignsController < ApplicationController
+	before_action :authenticate_user!
+    layout 'contact'
 	def index
+		@contact = Contact.find(params[:id])
+		if params[:loc]== "main"
+		else
+		@location = Location.find(params[:loc])
+		end
 	end
 end
