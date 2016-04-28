95ec9dd5330acf0777c974a56f458df8dbdd3390
Shane Wierenga <shane@midwestsignco.com>
2016-04-28 16:39:34 -0400

    current signs option

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -3,12 +3,31 @@ class SignsController < ApplicationController
     layout 'contact'
 	def index
 		@contact = Contact.find(params[:id])
+
 		if params[:loc]== "main"
 		else
 		@location = Location.find(params[:loc])
 		end
 	end
+	def show
+		
+		@sign= Sign.where(contact_id: params[:id], location_id: params[:loc])
+	end
+
 	def new
         @sign = Sign.new
     end
+    def create
+    	 
+    	 @sign = Sign.new(sign_params)
+    	 @contact = @sign.contact_id
+         if @sign.save
+      		redirect_to contact_path(:id => @sign.contact_id), status: 303, format: 'js'
+       
+     	 end
+	end
 end
+private
+    def sign_params
+      params.require(:sign).permit(:signDesc, :signLoc, :active, :builtBy, :installDate, :contact_id, :location_id)
+    end
--- app/models/sign.rb
+++ app/models/sign.rb
@@ -1,4 +1,5 @@
 class Sign < ApplicationRecord
 	belongs_to :contact
 	belongs_to :location, optional: true
+	scope :matchesContactandLocation, ->(id){ where(contact_id: id)}
 end
