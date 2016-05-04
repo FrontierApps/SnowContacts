74f4abae635e46e0eb92dd73aaad70d63d0a6632
shane wierenga <shane@midwestsignco.com>
2016-05-03 22:27:53 -0400

    user/employee functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,54 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  layout 'user'
 
   def index
   	@users=User.all
+  	if params[:id]
+      @user = User.find(params[:id])
+          respond_to do |format|
+            format.html { render "show" }
+            format.js { render "show" }
+          end
+      else
+      end
   end
 
+  def show
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.html { render "show" }
+      format.js { render "show" }
+    end
+  end
 
-end
+	
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+		respond_to do |format|
+		format.js { render "show" }
+		end
+
+		else 
+
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+
+		redirect_to user_path
+	end
+
+	private
+	def user_params
+	params.require(:user).permit(:email, :firstname, :lastname, :admin, :active)
+	end
+	end
