59f9c938256c9c09d44bb3099125e52df768f202
Shane Wierenga <shane@midwestsignco.com>
2016-05-10 16:38:51 -0400

    instead of showing timerecords under user I moved it to timerecords

--- app/controllers/timerecords_controller.rb
+++ app/controllers/timerecords_controller.rb
@@ -1,5 +1,13 @@
 class TimerecordsController < ApplicationController
   def index
-
+  	  @timerecords = current_user.timerecords.timethisweek.decorate
+      if params[:id]
+      @contact = Contact.find(params[:user_id])
+          respond_to do |format|
+            format.html { render "show" }
+            format.js { render "show" }
+          end
+      else
+      end
   end
 end
